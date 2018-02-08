92a5b6a96306c439b9e7cd3e4bb9a01d9cb709b5
ehu <fehuehuf@gmail.com>
2018-02-08 16:01:01 +0900

    ウィッシュリストURLの登録の実装

--- app/src/main/java/com/example/ehu/amazon_wishlist/MainActivity.java
+++ app/src/main/java/com/example/ehu/amazon_wishlist/MainActivity.java
@@ -4,17 +4,20 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //フローティングボタンのタップ実装
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,7 +35,7 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
-
+        //ナビゲーションドロワーの実装
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -81,9 +85,11 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_bookmark_border) {
+            Log.d(TAG, "home Selected!");
 
+
+        } else if (id == R.id.nav_bookmark_border) {
+            Log.d(TAG, "bookmark Selected!");
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
--- app/src/main/java/com/example/ehu/amazon_wishlist/WishlistRegistration.java
+++ app/src/main/java/com/example/ehu/amazon_wishlist/WishlistRegistration.java
@@ -2,12 +2,83 @@ package com.example.ehu.amazon_wishlist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Locale;
 
 public class WishlistRegistration extends AppCompatActivity {
+
+    private EditText urlEditText;
+    private String TAG = "WishlistRegistration";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wishlist_registration);
+
+        urlEditText = findViewById(R.id.URLEditText);
+        String json = getJson(urlEditText.getText().toString());
+        Log.d(TAG, json);
+
     }
+
+    public String getJson(String planUrl) {
+        URL reqestUrl = null;
+        HttpURLConnection connection = null;
+        try {
+            reqestUrl =
+                    new URL("http://ryouchi.usamimi.info/expandurl/index.php?callback=getOrgURL&url=" + planUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "不正なURLです。", Toast.LENGTH_SHORT).show();
+        }
+        try {
+            connection = (HttpURLConnection) reqestUrl.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "接続先が存在しません", Toast.LENGTH_SHORT).show();
+        }
+        //接続タイムアウトを設定する。
+        connection.setConnectTimeout(100000);
+        //レスポンスデータ読み取りタイムアウトを設定する。
+        connection.setReadTimeout(100000);
+        //ヘッダーにUser-Agentを設定する。
+        connection.setRequestProperty("User-Agent", "Android");
+        //ヘッダーにAccept-Languageを設定する。
+        connection.setRequestProperty("Accept-Language", Locale.getDefault().toString());
+        //ヘッダーにContent-Typeを設定する
+        connection.addRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        //HTTPのメソッドをPOSTに設定する。
+        try {
+            connection.setRequestMethod("POST");
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+            Log.d(TAG, "methodが指定できません");
+        }
+        //リクエストのボディ送信を許可する
+        connection.setDoOutput(true);
+        //レスポンスのボディ受信を許可する
+        connection.setDoInput(true);
+
+        OutputStream outputStream = null;
+        try {
+            connection.connect();
+            outputStream = connection.getOutputStream();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return outputStream.toString();
+    }
+
 }
